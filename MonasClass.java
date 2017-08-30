efbce537218e1d05847bf332db800123ffddf477
MonaRifaat <mona.rifaat@hotmail.com>
2017-08-30 22:33:01 +0200

    Third Requirement

--- MonasClass.java
+++ MonasClass.java
@@ -1,18 +1,17 @@
 public class MonasClass {
 
-	public static void main(String[] args) {
-		
-		if (args.length > 1)
-		{
-		   try {
-			throw new Exception("Program accepts only one argument");
-		} catch (Exception e) {
-			System.out.println("Program accepts only one int extra argument");
-			System.exit(1);
-		}
- 	
-		}
-		else if(args.length != 0)
+public static void main(String[] args) {
+	if (args.length > 1)
+	{
+	   try {
+		throw new Exception("Program accepts only one argument");
+	} catch (Exception e) {
+		System.out.println("Program accepts only one int extra argument");
+		System.exit(1);
+	}
+	
+	}
+	else if(args.length != 0)
 		  {
 			  MonasClass obj = new MonasClass();
 		      obj.PrintFirstAgrument(args[0]);
@@ -26,7 +25,16 @@ public void PrintFirstAgrument(String Argument)
 	try {	 
 	    	int num = 0;
 	        num = Integer.parseInt(Argument);
-	        System.out.println(num);
+	        
+	        if(num > 1)
+	        {
+		        for(int i=1; i<= num; i++)
+		        	System.out.println(i);
+	        }
+	        else
+	        	for(int i=num; i<= 1; i++)
+		        	System.out.println(i);
+	        	
 		}
 catch (NumberFormatException nfe) {
     System.out.println("The extra argument must be an integer.");
