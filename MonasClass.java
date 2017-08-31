6b41e517cc6e98f864a18063e7bc1606f805230a
MonaRifaat <mona.rifaat@hotmail.com>
2017-08-31 11:39:33 +0200

    Fourth Requirement - Modified

--- MonasClass.java
+++ MonasClass.java
@@ -1,29 +1,34 @@
 public class MonasClass {
 
-public static void main(String[] args) {
+public static void main(String[] args) 
+{
 		
 	if (args.length > 1)
 	{
-	   try {
+	   try 
+	   {
 		throw new Exception("Program accepts only one argument");
-	} catch (Exception e) {
+	   } 
+	   catch (Exception e) 
+	   {
 		System.out.println("Program accepts only one int extra argument");
 		System.exit(1);
-	}
+	   }
 	
 	}
 	else if(args.length != 0)
-		  {
-			  MonasClass obj = new MonasClass();
-		      obj.PrintFirstAgrument(args[0]);
-		  }		
+	{
+		MonasClass obj = new MonasClass();
+		obj.PrintFirstAgrument(args[0]);
+	}		
 
 
-	}
+}
 
 public void PrintFirstAgrument(String Argument)
 {
-	try {	 
+	try
+	{	 
 	    	int num = 0;
 	        num = Integer.parseInt(Argument);
 	        
@@ -31,7 +36,9 @@ public void PrintFirstAgrument(String Argument)
 	        {
 		        for(int i=1; i<= num; i++)
 		        {
-		        	if (i% 3 == 0)
+		        	if ((i% 3 == 0) && (i%5 == 0))
+						System.out.println("FizzBuzz");
+	        		else if (i% 3 == 0)
 		        		System.out.println("Fizz");
 		        	else if (i%5 == 0)
 		        		System.out.println("Buzz");
@@ -39,22 +46,26 @@ public void PrintFirstAgrument(String Argument)
 		        		System.out.println(i);
 		        }
 	        }
+		
 	        else
-	        	for(int i=num; i<= 1; i++)
+			for(int i=num; i<= 1; i++)
 		        {
-	        		if (i% 3 == 0)
+					if ((i% 3 == 0) && (i%5 == 0))
+						System.out.println("FizzBuzz");
+	        		else if (i% 3 == 0)
 		        		System.out.println("Fizz");
 		        	else if (i%5 == 0)
 		        		System.out.println("Buzz");
 		        	else
 		        		System.out.println(i);
-		        }
-	        	
-		}
-catch (NumberFormatException nfe) {
-    System.out.println("The extra argument must be an integer.");
-    System.exit(1);
-}
+		        }     	
+	}
+	
+    catch (NumberFormatException nfe) 
+    {
+    	System.out.println("The extra argument must be an integer.");
+    	System.exit(1);
+    }
 }
 
 }
